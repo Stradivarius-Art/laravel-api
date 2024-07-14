fa02dbd1ec897b62bce67d4ca14ed6892ea8a377
artemlepesch <86205531+artemlepesch@users.noreply.github.com>
2024-07-14 16:21:44 +0300

    Добавил mixin в качестве подсказок для ApiResource

--- app/Http/Resources/Posts/GetPosts.php
+++ app/Http/Resources/Posts/GetPosts.php
@@ -5,6 +5,9 @@ namespace App\Http\Resources\Posts;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
+/**
+ * @mixin \App\Models\Post
+ */
 class GetPosts extends JsonResource
 {
     public function toArray(Request $request): array
